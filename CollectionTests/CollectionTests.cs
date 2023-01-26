8f6768aeb1aa01db6ad9ac6dc5e383c213d61d0e
test <test@testt.lab>
2023-01-26 11:55:48 +0200

    added new test

--- CollectionTests/CollectionTests.cs
+++ CollectionTests/CollectionTests.cs
@@ -17,8 +17,20 @@ namespace CollectionTests
         [Test]
         public void Test_Collection_ConstructorSingleItem()
         {
-            var nums = new Collection<int>(5);
-            Assert.That(nums[0], Is.EqualTo(5));
+            var nums = new Collection<int>(6);
+            Assert.That(nums[0], Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Test_Collection_GetByIndex()
+        {
+            // Arrange
+            var strings = new Collection<int>(1,2,3,4,5,6,7);
+
+            Console.WriteLine(strings[2].ToString());
+
+            // Assert
+            Assert.That(strings[3].ToString, Is.EqualTo("4"));
         }
     }
 }
